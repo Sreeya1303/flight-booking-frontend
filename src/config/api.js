5fff88bec3445cfe93638559da906ab864e825c8
Sreeya@1303 <sreeksreeya@gmail.com>
2025-12-23 15:03:57 +0530

    Fix API base URL for production

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,6 @@
-const API_BASE_URL = "https://flight-booking-backend-k9xl.onrender.com";
+const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://flight-booking-backend-k9xl.onrender.com";
+
 export default API_BASE_URL;
